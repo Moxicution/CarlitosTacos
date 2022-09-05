e6b98ee730f475c651ed1e02412ee2b29ac2098d
Carlos Schipper <carlos.schipper@soundofdata.nl>
2022-09-06 00:42:43 +0200

    Added: git ig., proj. stuff and main game window

--- CarlitosTacos/source/Main.hx
+++ CarlitosTacos/source/Main.hx
@@ -1,6 +1,8 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.util.FlxSave;
 import openfl.display.Sprite;
 
 class Main extends Sprite
@@ -8,6 +10,13 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(320, 240, PlayState, 60, 60, true));
+
+		#if desktop
+		if (FlxG.save.data.fullscreen != null)
+		{
+			FlxG.fullscreen = FlxG.save.data.fullscreen;
+		}
+		#end
 	}
 }
